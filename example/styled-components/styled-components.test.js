ec70484f701b0fd205b6229e5a474109b7e68ec8
Brent <brent.clark@sportshubtech.com>
2017-12-05 22:36:09 -0600

    add serializer manually

--- example/styled-components/styled-components.test.js
+++ example/styled-components/styled-components.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import StyledComponent from './index';
-import 'jest-styled-components';
+import styleSheetSerializer from 'jest-styled-components/src/styleSheetSerializer';
+import { addSerializer } from '../../src/index';
 
 test('test that handles serializers', () => {
+  addSerializer(styleSheetSerializer);
   const tree = renderer.create(<StyledComponent />).toJSON();
   expect(tree).toMatchSpecificSnapshot('./index.shot');
 });
